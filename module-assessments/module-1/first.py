107b9c5ac4e41c13bb450a7ec4f6dec8b5c8eecf
Malek Eltassi <eltassimalek@gmail.com>
2025-09-08 12:04:11 -0600

    finished first module assessment

---
+++ module-assessments/module-1/first.py
@@ -0,0 +1,7 @@
+name = "Malek El Tassi"
+age = 22
+summary = "I want to improve my Python skills and build real projects."
+
+print("Hello, my name is", name + ".")
+print("I am", age, "years old.")
+print("In this project,", summary)
