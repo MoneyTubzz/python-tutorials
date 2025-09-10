72ecf46d64f2fbcd5b03d5062b6307ba47a4bff3
Malek Eltassi <eltassimalek@gmail.com>
2025-09-10 09:34:15 -0600

    finished prime number assessment

---
+++ module-assessments/prime-numbers/main.py
@@ -0,0 +1,11 @@
+num=int(input("Enter a number: "))
+print("Prime numbers between 2 and", num, ":")
+
+for n in range(2, num + 1):
+    is_prime = True
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            is_prime = False
+            break
+    if is_prime:
+        print(n)
